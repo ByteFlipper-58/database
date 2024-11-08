c820143f4003272eff8bf0cbbf4c037dfd9febf9
ByteFlipper <166491099+ByteFlipper-58@users.noreply.github.com>
2024-11-08 15:18:04 +0100

    Create apps_list.js

---
+++ apps_list.js
@@ -0,0 +1,11 @@
+{
+  "apps": [
+    {
+      "app_name": "FF Sensitivities",
+      "app_package": "com.byteflipper.ffsensitivities",
+      "version": "v.1.3.7",
+      "version_code": 40,
+      "apk_file": ""
+    }
+  ]
+}
